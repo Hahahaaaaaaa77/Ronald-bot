08b380e05bd041fd0a1c2be2e24aaa1da1051419
Hahahaaaaaaa77 <claraae04@gmail.com>
2024-12-08 23:29:51 +0000

    Update and rename image.js to gpt.js

--- commands/gpt.js
+++ commands/gpt.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
-  name: "image",
+  name: "gpt",
   description: "interact with yoru with generate & recognize image",
   author: "Emman Co",
 
